704953e48e057a06945e78b1e28688807107cd0e
lukarostan <47535948+lukarostan@users.noreply.github.com>
2020-05-01 23:51:18 +0200

    Added some functionality
    Button now does something
    Will have to investigate the timer, those are not seconds...

--- ReactionTester/Script.js
+++ ReactionTester/Script.js
@@ -1,1 +1,33 @@
-console.log("test")
+$(function() {
+      $("#reactButton").click( function()
+        {
+           $("#reactButton").html("Wait...");
+          setTimeout(
+            function() 
+            { 
+            //do something special
+            $("#mainWindow").css("background-color", "00838f");
+            $("#reactButton").html("CLICK ME!");
+            }, timer );
+        }
+      );
+});
+
+var timer = Math.random()*10 *Math.random() * 1213;
+console.log(timer);
+
+var ms = 0;
+var second = 0;
+
+    
+setInterval(clockRunning, 1);
+
+function clockRunning(){
+	
+	ms++;
+	if (ms == 1000){
+		second += 1;
+		ms = 0;}
+    
+    $("#timer").html(second +" : "+ ms);
+};
