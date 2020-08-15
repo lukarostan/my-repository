b7b416ff57ea814811866c43bcccefffe0ce610b
lukarostan <luka.rostan@factory.hr>
2020-08-15 19:59:00 +0200

    commit

--- ReactionTesterV2/testScript.js
+++ ReactionTesterV2/testScript.js
@@ -1,1 +1,8 @@
-("#footer").html.set("sdd");
+var t = new Date().getTime();
+        console.log(t);
+        document.getElementById("shape").onclick = function (){
+            var time = new Date().getTime() - t;
+                
+            document.getElementById("shape").style.display = "none";
+                console.log("time:"+time);
+        }
