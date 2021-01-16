3bc62302e32c90952e003c48a2208135c0b994ae
lukarostan <lukarostan@gmail.com>
2021-01-16 14:41:50 +0100

    cleaned up repo

--- ReactionTesterV2/testScript.js
+++
@@ -1,8 +0,0 @@
-var t = new Date().getTime();
-        console.log(t);
-        document.getElementById("shape").onclick = function (){
-            var time = new Date().getTime() - t;
-                
-            document.getElementById("shape").style.display = "none";
-                console.log("time:"+time);
-        }
